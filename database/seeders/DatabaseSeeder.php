eea8d5822d193194f87c4694eaeb820ff9c809b8
Ahmed Aziz Dimassi <hullolmalex@gmail.com>
2026-05-07 11:20:35 +0100

    fix: add missing Laravel core files (artisan, bootstrap, config, public, storage, database)

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Seeders will be added in STEP 2
+    }
+}
